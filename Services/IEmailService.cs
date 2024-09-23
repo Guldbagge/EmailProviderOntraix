437060bc1f749d64e9f946cebd17aff7f3f40355
Edvin Thörnholm <edvin.thornholm@gmail.com>
2024-09-23 10:46:11 +0200

    working

---
+++ Services/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Messaging.ServiceBus;
+using EmailProviderOntraix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailProviderOntraix.Services;
+
+public interface IEmailService
+{
+    bool SendEmail(EmailRequest emailRequest);
+    EmailRequest UnpackEmailRequest(ServiceBusReceivedMessage message);
+}
+
